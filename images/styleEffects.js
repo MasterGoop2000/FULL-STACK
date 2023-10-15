e37e384a5e52e185a7437e1291279aae7b4552c5
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 20:58:58 +1000

    fixed error 6.0

--- images/styleEffects.js
+++ images/styleEffects.js
@@ -2,10 +2,10 @@
 let color1 = "black";
 setInterval(function(){
     if(color == "black"){
-        document.getElementById("title").style.color = "white";
+        document.getElementById("mainT").style.color = "white";
         color = "white";
     } else {
-        document.getElementsById("title").style.color = "black";
+        document.getElementsById("mainT").style.color = "black";
         color1 = "black";
     }
 }, 1000);
