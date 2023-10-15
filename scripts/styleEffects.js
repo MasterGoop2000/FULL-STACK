1e5c293496a03ce175f27c348ab25ea1175579a3
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 10:05:28 +1000

    attempting to fix js style effects

--- scripts/styleEffects.js
+++ scripts/styleEffects.js
@@ -3,7 +3,9 @@ let color = "black";
 setInterval(function(){
     if(color == "black"){
         document.getElementsByClassName("title").style.color = "white";
+        color = "white";
     } else {
         document.getElementsByClassName("title").style.color = "black";
+        color = "black";
     }
 }, 1000);
