7c677c16f7c8b7e88fdd92dc2b0e84f6d29ea74c
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 20:54:44 +1000

    absolute fix js

--- images/styleEffects.js
+++ images/styleEffects.js
@@ -2,13 +2,13 @@
 let color1 = "black";
 setInterval(function(){
     if(color == "black"){
-        let thing = document.getElementsByClassName("title").style.color = "white";
+        let thing = document.getElementsByClassName("title");
         for(var i = 0; i < thing.length; i++){
             collection[i].style.color = "white";
         }
         color1 = "white";
     } else {
-        let thing = document.getElementsByClassName("title").style.color = "white";
+        let thing = document.getElementsByClassName("title");
         for(var i = 0; i < thing.length; i++){
             collection[i].style.color = "black";
         }
