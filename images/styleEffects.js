0e62ae85ce09bff0618330800d5550a932077b90
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 20:58:02 +1000

    fix 5.0 js

--- images/styleEffects.js
+++ images/styleEffects.js
@@ -1,18 +1,11 @@
 // JavaScript Code Here
 let color1 = "black";
-let thing = document.getElementsByClassName("title");
 setInterval(function(){
     if(color == "black"){
-        
-        for(var i = 0; i < thing.length; i++){
-            collection[i].style.color = "white";
-        }
-        color1 = "white";
+        document.getElementById("title").style.color = "white";
+        color = "white";
     } else {
-        
-        for(var i = 0; i < thing.length; i++){
-            collection[i].style.color = "black";
-        }
+        document.getElementsById("title").style.color = "black";
         color1 = "black";
     }
 }, 1000);
