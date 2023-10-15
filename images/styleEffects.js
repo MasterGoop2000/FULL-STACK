270a2047a16eade0011fc9a76050c636ede5aabc
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 20:53:23 +1000

    fix 2.0 js

--- images/styleEffects.js
+++ images/styleEffects.js
@@ -1,17 +1,17 @@
 // JavaScript Code Here
-let color = "black";
+let color1 = "black";
 setInterval(function(){
     if(color == "black"){
         let thing = document.getElementsByClassName("title").style.color = "white";
         for(var i = 0; i < thing.length; i++){
             collection[i].style.color = "white";
         }
-        color = "white";
+        color1 = "white";
     } else {
         let thing = document.getElementsByClassName("title").style.color = "white";
         for(var i = 0; i < thing.length; i++){
             collection[i].style.color = "black";
         }
-        color = "black";
+        color1 = "black";
     }
 }, 1000);
