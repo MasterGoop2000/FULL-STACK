776828dad727e6cc37c3f1f37bec4a5aa748e405
MasterGoop2000 <mastergoop2000@gmail.com>
2023-10-15 20:51:43 +1000

    js fix

--- images/styleEffects.js
+++ images/styleEffects.js
@@ -2,10 +2,16 @@
 let color = "black";
 setInterval(function(){
     if(color == "black"){
-        document.getElementsByClassName("title").style.color = "white";
+        let thing = document.getElementsByClassName("title").style.color = "white";
+        for(var i = 0; i < thing.length; i++){
+            collection[i].style.color = "white";
+        }
         color = "white";
     } else {
-        document.getElementsByClassName("title").style.color = "black";
+        let thing = document.getElementsByClassName("title").style.color = "white";
+        for(var i = 0; i < thing.length; i++){
+            collection[i].style.color = "black";
+        }
         color = "black";
     }
 }, 1000);
